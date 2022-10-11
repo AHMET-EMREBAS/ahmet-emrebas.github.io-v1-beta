1455588bcdc19f70c84b49e881a9eafe36f48bd9
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-10-10 21:16:41 -0500

    clean up

--- apps/pos-client/pos-desktop/src/app/constants.ts
+++
@@ -1,4 +0,0 @@
-export const rendererAppPort = 4200;
-export const rendererAppName = 'pos-web';
-export const electronAppName = 'pos-client-pos-desktop';
-export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
