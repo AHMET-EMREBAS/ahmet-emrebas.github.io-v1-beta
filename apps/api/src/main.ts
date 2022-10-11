8890a8f8ac1b345e3ef34da08bb587e0519c40c3
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-10-10 22:07:28 -0500

    serve favicon

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,9 @@
  * This is only a minimal backend to get started.
  */
 
+import { join } from 'path';
+import * as favicon from 'serve-favicon';
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
@@ -13,6 +16,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  app.use(favicon(join(__dirname, 'assets', 'favicon.ico')));
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
