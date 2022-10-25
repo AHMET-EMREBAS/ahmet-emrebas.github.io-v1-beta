c723991f7f1bcf57f360bb2a65f48c88150daf31
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-10-24 21:15:45 -0500

    locale configuration

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,9 +1,5 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import {
-  LOCALE_ID,
-  NgModule,
-} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -50,16 +46,6 @@ const routes: Routes = [
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-  ],
-  providers: [
-    {
-      deps: [LOCALE_ID],
-      provide: APP_BASE_HREF,
-      multi: false,
-      useFactory: (locale: string) => {
-        return `/web/${locale}`;
-      },
-    },
   ],
   bootstrap: [AppComponent],
 })
