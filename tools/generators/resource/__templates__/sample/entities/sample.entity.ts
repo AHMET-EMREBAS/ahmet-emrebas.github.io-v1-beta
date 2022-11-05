9271501fc7b73ead0f09d2b4b23edce919c8fe0c
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-11-04 21:53:40 -0500

    clean up

--- tools/generators/resource/__templates__/sample/entities/sample.entity.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from 'core';
-import { Column, Entity } from 'typeorm';
-
-@Entity()
-export class Sample extends BaseEntity {
-  @Column({ type: 'text', nullable: true }) sample: string;
-}
