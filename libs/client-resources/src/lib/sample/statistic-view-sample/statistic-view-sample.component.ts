e67040e451125ee9f6c54081916b48e2157200fc
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-11-06 01:27:11 -0500

    mobile responsive form

--- libs/client-resources/src/lib/sample/statistic-view-sample/statistic-view-sample.component.ts
+++ libs/client-resources/src/lib/sample/statistic-view-sample/statistic-view-sample.component.ts
@@ -3,22 +3,30 @@ import {
   OnInit,
 } from '@angular/core';
 
+import { ChartData } from 'chart.js';
+
 @Component({
   selector: 'ae-statistic-view-sample',
   templateUrl: './statistic-view-sample.component.html',
   styleUrls: ['./statistic-view-sample.component.scss'],
 })
 export class StatisticViewSampleComponent implements OnInit {
-  data = {
+  data: ChartData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+
     datasets: [
       {
         label: 'First Dataset',
+        backgroundColor: [
+          'orange',
+          'blue',
+          'yellow',
+          'teal',
+          'cyan',
+          'purple',
+          'royalblue',
+        ],
         data: [65, 59, 80, 81, 56, 55, 40],
-      },
-      {
-        label: 'Second Dataset',
-        data: [28, 48, 40, 19, 86, 27, 90],
       },
     ],
   };
--- libs/client-resources/src/lib/sample/statistic-view-sample/statistic-view-sample.module.ts
+++ libs/client-resources/src/lib/sample/statistic-view-sample/statistic-view-sample.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
 
 import {
@@ -13,6 +14,7 @@ import {
   imports: [
     CommonModule,
     ChartModule,
+    CardModule,
     RouterModule.forChild([
       {
         path: '',
