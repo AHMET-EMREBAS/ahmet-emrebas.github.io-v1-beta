7b96a615378da87f277cba32ae76ff8b4ae53803
AHMET-EMREBAS <108595803+AHMET-EMREBAS@users.noreply.github.com>
2022-11-10 17:32:29 -0600

    initial

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,41 +1,14 @@
-import { AuthModule } from 'auth';
 import { join } from 'path';
 
-import { CacheModule, Module } from '@nestjs/common';
+import {
+  CacheModule,
+  Module,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { CategorySubscriber, CategoryModule } from 'rest/inventory/category';
-import {
-  DepartmentSubscriber,
-  DepartmentModule,
-} from 'rest/inventory/department';
-import { FeatureSubscriber, FeatureModule } from 'rest/inventory/feature';
-import { LocationSubscriber, LocationModule } from 'rest/inventory/location';
-import { OrderSubscriber, OrderModule } from 'rest/inventory/order';
-import {
-  PermissionSubscriber,
-  PermissionModule,
-} from 'rest/inventory/permission';
-import { PriceSubscriber, PriceModule } from 'rest/inventory/price';
-import {
-  PricelevelSubscriber,
-  PricelevelModule,
-} from 'rest/inventory/pricelevel';
-import { ProductSubscriber, ProductModule } from 'rest/inventory/product';
-import { QuantitySubscriber, QuantityModule } from 'rest/inventory/quantity';
-import { ResourceSubscriber, ResourceModule } from 'rest/inventory/resource';
-import { SkuSubscriber, SkuModule } from 'rest/inventory/sku';
-import { StoreSubscriber, StoreModule } from 'rest/inventory/store';
-import {
-  TransactionSubscriber,
-  TransactionModule,
-} from 'rest/inventory/transaction';
-import { UserSubscriber, UserModule } from 'rest/inventory/user';
-import { VariantSubscriber, VariantModule } from 'rest/inventory/variant';
 
 @Module({
   imports: [
@@ -61,60 +34,10 @@ import { VariantSubscriber, VariantModule } from 'rest/inventory/variant';
       type: 'better-sqlite3',
       database: 'tmp/database/main.sqlite',
       autoLoadEntities: true,
-      subscribers: [
-        CategorySubscriber,
-        DepartmentSubscriber,
-        FeatureSubscriber,
-        LocationSubscriber,
-        OrderSubscriber,
-        PermissionSubscriber,
-        PriceSubscriber,
-        PricelevelSubscriber,
-        ProductSubscriber,
-        QuantitySubscriber,
-        ResourceSubscriber,
-        SkuSubscriber,
-        StoreSubscriber,
-        TransactionSubscriber,
-        UserSubscriber,
-        VariantSubscriber,
-      ],
+      subscribers: [],
       synchronize: true,
       dropSchema: true,
     }),
-    AuthModule,
-
-    CategoryModule,
-
-    DepartmentModule,
-
-    FeatureModule,
-
-    LocationModule,
-
-    OrderModule,
-
-    PermissionModule,
-
-    PriceModule,
-
-    PricelevelModule,
-
-    ProductModule,
-
-    QuantityModule,
-
-    ResourceModule,
-
-    SkuModule,
-
-    StoreModule,
-
-    TransactionModule,
-
-    UserModule,
-
-    VariantModule,
   ],
 })
 export class AppModule {}
